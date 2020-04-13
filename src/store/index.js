8452215c76a4880890f8c0074fca502b9842287e
hdinjos <hdinjos@gmail.com>
2020-04-13 20:30:32 +0700

    commit object in methods

--- src/store/index.js
+++ src/store/index.js
@@ -5,15 +5,20 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    result: {}
+    result: {},
+    count: 0
   },
   mutations: {
     fetch(state, payload) {
       state.result = payload;
+    },
+    increment(state, payload) {
+      state.count += payload.amount;
     }
   },
   getters: {
-    result: state => state.result
+    result: state => state.result,
+    count: state => state.count
   },
   actions: {},
   modules: {}
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="increment">Hitung</button>
+    <p>{{count}}</p>
   </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      pesan: "test props"
+    };
+  },
+  methods: {
+    increment() {
+      this.$store.commit({
+        type: "increment",
+        amount: 2
+      });
+    }
+  },
+  computed: {
+    ...mapGetters(["count"])
+  }
+};
+</script>
